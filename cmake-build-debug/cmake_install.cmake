d16194dd6b773a60e29beadfef032df2b67ee79f
valerian <valerian.priou@ensea.fr>
2024-09-10 17:29:16 +0200

    Tri effectué

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Valérian/Desktop/Bureau/ENSEA/3A/ESE/Module_0/Rappel_C
+# Install script for directory: C:/Rappel_C
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Valérian/Desktop/Bureau/ENSEA/3A/ESE/Module_0/Rappel_C/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Rappel_C/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,6 +1,81 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define taille 10
+
+int random_number(){
+    int number = rand() % 100;
+    return number;
+}
+
+int* montrer(int* tableau){
+    for(int i = 0; i < taille; i++){
+        printf("%d ",tableau[i]);
+    }
+    printf("\n\n");
+}
+
+char* calcul_maximal(int* tableau){
+    static char resultat[100];
+    int number = tableau[0];
+    int indice = 0;
+    for (int i = 0; i < taille  ; i++){
+        if (tableau[i] > number) {
+            number = tableau[i];
+            indice = i;
+        }
+    }
+    sprintf(resultat, "Le maximum du tableau est : %d et son indice est : %d.\n",number,indice);
+    return resultat;
+}
+
+char* calcul_minimal(int* tableau){
+    static char resultat[100];
+    int number = tableau[0];
+    int indice = 0;
+    for (int i = 0; i < taille  ; i++){
+        if (tableau[i] < number) {
+            number = tableau[i];
+            indice = i;
+        }
+    }
+    sprintf(resultat, "Le minimum du tableau est : %d et son indice est : %d.\n",number,indice);
+    return resultat;
+}
+
+int* trier(int* tableau){
+    for (int i = 0; i < taille; i++){
+        for (int j = 0; j < taille; j++){
+            if(tableau[j] > tableau[j+1]){
+                int temp = tableau[j];
+                tableau[j] = tableau[j+1];
+                tableau[j+1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    srand(time(0));
+    int tableau[taille];
+
+    for(int i = 0; i < taille; i++){
+        tableau[i] = random_number();
+    }
+
+    printf("\nLe contenu du tableau est le suivant :\n\n");
+
+    montrer(tableau);
+
+    printf("%s",calcul_maximal(tableau));
+    printf("%s",calcul_minimal(tableau));
+
+    printf("\n");
+
+    trier(tableau);
+
+    montrer(tableau);
+
     return 0;
 }
